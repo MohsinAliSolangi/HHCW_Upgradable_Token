c9a5d4d70ca2aebb1a2cafdfd99b911f2651e025
“MohsinPluton” <Mohsin.pluton@gmail.com>
2023-04-08 06:17:03 +0500

    setProxy in contract

--- contracts/hhcwToken.sol
+++ contracts/hhcwToken.sol
@@ -1,26 +1,83 @@
-
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 error totalSupplyExceed();
-
-contract hhcwToken is ERC20 , Ownable {
-    constructor() ERC20("HHCWToken", "HHCWToken") {}
+error waitForStartSale();
+error SaleSupplyExced();
+error pleaseSendTokenPrice();
+contract MyToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
     uint256 public supply = 20_000_000 ether;
+    uint256 public preSale = 5_000_000 ether;
+    uint256 public Sale = 5_000_000 ether;
+    uint256 public mintPrice = 1;
+    
+    bool public sale;
+    bool public presale;
+
+ 
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC20_init("HHCWToken", "HHCWToken");
+        __Ownable_init();
+      
+    }
+    
+    function startSale()public onlyOwner{
+        presale=false;
+        sale=true;
+    }
+
+    function startPreSale()public onlyOwner{
+        sale=false;
+        presale=true;
+    }
 
     function increaseTotalSupply(uint256 _supply) public onlyOwner {
        supply +=_supply;
     }
+
+    function increaseMintPrice(uint256 _mintPrice) public onlyOwner {
+       mintPrice +=_mintPrice;
+    }
     
-    function mint(address to, uint256 _amount) public onlyOwner {
+    function mint(address to, uint256 _amount) public payable onlyOwner {
+    if(sale == false && presale == false){
+        revert waitForStartSale();
+    }
     if ((totalSupply() + _amount) > supply) {
-            revert totalSupplyExceed();
-        }
-        _mint(to,_amount);
+        revert totalSupplyExceed();
+    }
+
+    if(presale!=false){
+    if((totalSupply() + _amount)>(Sale)){
+        revert SaleSupplyExced();
+    }
+    if(msg.value < (_amount*mintPrice)) {
+        revert pleaseSendTokenPrice();   
+    }
+     
+      _mint(to, _amount);
+    }
+
+    if(sale!=false){
+    if((totalSupply() + _amount)>(Sale)){
+        revert SaleSupplyExced();
+    }
+    if(msg.value < (_amount*mintPrice)) {
+        revert pleaseSendTokenPrice();   
+    }
+     
+      _mint(to, _amount);
+    }
+
     }
 
     function airdrop(address[] memory recipients, uint256[] memory amounts) public onlyOwner {
@@ -36,5 +93,4 @@ contract hhcwToken is ERC20 , Ownable {
             _mint(recipient,amount);
         }
     }
-
 }
